2c413df2b7187b4ec5e8cf52ad6f338bdb558cf4
Shivam Menda <shivammenda2002@gmail.com>
2023-03-22 18:03:56 +0530

    Rest Framework setup done.

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(["GET","POST"])
+def homepage(request:Request):
+    if(request.method=="POST"):
+        data=request.data
+        response={"message":"hello","data":data}
+        return Response(response,status=status.HTTP_201_CREATED)
+    response={"message":"hello"}
+    return Response(response,status=status.HTTP_200_OK)
+
+    
+
+
+
+
+
